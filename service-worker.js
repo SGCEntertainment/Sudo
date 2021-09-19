20f06cf236e154495701a29916f4bfb361bcff88
SGCEntertainment <44565516+SGCEntertainment@users.noreply.github.com>
2021-09-20 00:16:50 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,7 @@
-﻿
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+      caches.match(event.request).then(function(response) {
+          return response || fetch(event.request);
+      })
+  );
+});
